9fb2641f02a6162ba41e43f9902e98a06abd7192
João Enrique Barbosa Santos Alves <99426704+JoaoEnrique13@users.noreply.github.com>
2022-08-03 11:03:53 -0300

    Jogo do mario personalizado

--- mario/js/script.js
+++ mario/js/script.js
@@ -1,73 +1,83 @@
-const mario = document.querySelector(".mario")//mario
+const joao = document.querySelector(".joao")//personagem
 const piper = document.querySelector(".pipe")//cano
-const clouds = document.querySelector(".clouds")
-const clouds1 = document.querySelector(".clouds1")
-const musicPlay = document.querySelector("#play-music")
-const musicOver = document.querySelector("#game-over-music")
+const clouds = document.querySelector(".clouds")//nuvem
+const clouds1 = document.querySelector(".clouds1")//nuvem
+const musicPlay = document.querySelector("#play-music")//audio do jogo
+const musicOver = document.querySelector("#game-over-music")//audio ao perder
 
 //funcao para pular
 const jump = () =>{
-    mario.classList.add("jump")//adcionar classe jump
-    mario.src = "img/jump.png"
+    joao.classList.add("jump")//adcionar classe jump
+    joao.src = "img/jump.png"
     //remover classe jump depois de um tempo
     setTimeout(()=>{
-        mario.classList.remove("jump")
-        mario.src = "img/joao.gif"
+        joao.classList.remove("jump")
+        joao.src = "img/joao.gif"
     },500)
 }
 
+//funcao loop que é chamada a cada 10ms para verificar se o jogo acabou
 const loop = setInterval(()=>{
-    const piperPosition = piper.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
-    const cloudsPosition = + window.getComputedStyle(clouds).right.replace('px', '')
-    const cloudsPosition1 = + window.getComputedStyle(clouds1).right.replace('px', '')
-    musicPlay.play()
+    const piperPosition = piper.offsetLeft;//posicao do cano na esquerda
+
+    //o mais(+) antes do window converte para numero se for possivel
+    const joaoPosition = +window.getComputedStyle(joao).bottom.replace('px', '')//pegar a posicao do personagem do bottom e retirar o px para converter para numero
+    const cloudsPosition = + window.getComputedStyle(clouds).right.replace('px', '')//pegar a posicao da nuvem a direita e retirar o px para converter para numero
+    const cloudsPosition1 = + window.getComputedStyle(clouds1).right.replace('px', '')//pegar a posicao da nuvem a direita e retirar o px para converter para numero
+    //musicPlay.play()//tocar musica ao comecar o jogo
     
-    if(piperPosition < 120 && marioPosition <= 80 && piperPosition >0){
-        piper.style.animation = "none"
-        piper.style.left = `${piperPosition}px`
+    //verifica se o personagem bateu no objeto
+    if(piperPosition < 120 && joaoPosition <= 70 && piperPosition >0){
+        piper.style.animation = "none"//tira animacao
+        piper.style.left = `${piperPosition}px`//para o cano na ultima posicao dele
 
-        mario.src = "img/game-over.png"
-        mario.style.marginLeft = "50px"
-        mario.classList.add("game-over")
-        mario.style.bottom ="-250px"
+        joao.src = "img/game-over.png"//mudar imagem do personagem
+        joao.style.marginLeft = "50px"//alinha o personagem
+        joao.classList.add("game-over")//adicionar classe com a animacao de game over
+        joao.style.bottom ="-250px"//deixa o personagem fora da tela
 
-        clouds.style.animation = "none"
-        clouds1.style.animation = "none"
+        clouds.style.animation = "none"//tira a animacao da nuvem
+        clouds1.style.animation = "none"//tira a animacao da nuvem
         
-        clouds.style.right = `${cloudsPosition}px`
-        clouds1.style.right = `${cloudsPosition1}px`
+        clouds.style.right = `${cloudsPosition}px`//deixa nuvem na ultima posicao dela
+        clouds1.style.right = `${cloudsPosition1}px`//deixa nuvem na ultima posicao dela
         
 
-        const tempo = setInterval(gameOver, 1000)
+        const tempo = setInterval(gameOver, 1000)//chama a funcao gameOver depois de um segundo com a tela de game over
 
-        clearInterval(loop)
-        clearInterval(timer)
+        clearInterval(loop)//para a funcao loop
+        clearInterval(timer)//para a funcao timer
 
-        musicPlay.pause()
-        musicOver.play()
+        musicPlay.pause()//pausar musica do jogo
+        musicOver.play()//tocar musica de game over
     }
 
 }, 10)
 
-m=0
-s=1
+//funcao timer (conta o tempo na tela)
+m=0//minutos
+s=1//segundos
 const timer = setInterval(() =>{
+    //caso o segundo for <9 concatenar com 0 para deixar no padrao 00
     if(s<=9){
         s = "0" + s
     }
     
+    //quando os segundos chegar no 59, zera o segundo e incrementa os minutos
     if(s >= 59){
         s=0;
         m = m + 1
     }
 
+    //caso o minuto for <9 concatenar com 0 para deixar no padrao 00
     if(m <=9){
         m = "0" + m
     }
+    //colocar segundos e minutos na tela
     document.querySelector(".segundo").innerHTML = s
     document.querySelector(".minuto").innerHTML =  m
 
+    //nao aparecer minutos na tela de game over se os minutos for 0
     if(m <=0){
         document.querySelector(".recorde-segundos").innerHTML = s
     }
@@ -76,21 +86,25 @@ const timer = setInterval(() =>{
         document.querySelector(".recorde-minutos").innerHTML =  m + " minutos e "
     }
     
+    //incrementa segundo
     s ++
-    m = +m + 0
+    m = +m + 0//converte os minutos para inteiro
 }, 1000)
 
-buttonRecomecar = document.querySelector("button")
+//funcao recomecar 
+buttonRecomecar = document.querySelector("button")//botao da tela gameOver
 buttonRecomecar.addEventListener("click", ()=>{
-    location. reload();
+    location. reload();//recarregar site
 })
 
+//funcao GameOver, mostra a tela de gameOver
 function gameOver(){
-    document.querySelector(".tela-game-over").style.display = "block"
-        const fundo = document.querySelector(".game-board")
-        const chao = document.querySelector(".chao")
-        const pipe = document.querySelector(".pipe")
+    document.querySelector(".tela-game-over").style.display = "block"//mostra a tela de game over
+        const fundo = document.querySelector(".game-board")//pega valor do fundo
+        const chao = document.querySelector(".chao")//pega valor do chao
+        const pipe = document.querySelector(".pipe")//pega valor do cano
 
+        //adiciona classe fundo-game-over nos itens para borrar e escurecer o fundo
         fundo.classList.add("fundo-game-over")
         chao.classList.add("fundo-game-over")
         pipe.classList.add("fundo-game-over")
@@ -101,5 +115,6 @@ function gameOver(){
 //ativar funcao pular ao clicar em qualquer tecla
 document.addEventListener("keydown", jump)
 
+//chamar funcao de pulo ao clicar na tela
 body = document.querySelector("body")
 body.addEventListener("click", jump)
